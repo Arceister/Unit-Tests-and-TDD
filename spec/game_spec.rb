8f525f5b6811d99d5ea97f47b5911d3e5ae29410
arceister <imandajagad01@gmail.com>
2022-04-08 17:01:59 +0700

    Test: Green `game_spec.rb`

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,29 +1,20 @@
-require 'game'
+class Game
+  # Now it's necessary to assign secret_word to @secret_word
+  def initialize(secret_word)
+    @secret_word = secret_word
+  end
 
-describe 'Game' do
-  describe '#match_word' do
-    it 'should handle all incorrect letters well' do
-      game = Game.new('DRINK')
-      guess_word = 'HELLO'
+  def match_word(guess_word)
+    result = "\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}"
 
-      result = game.match_word(guess_word)
-
-      expect(result).to eq("\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}")
+    # Now it's necessary to properly handle a correct letter in incorrect position
+    guess_word.split('').each_with_index do |letter, index|
+      if @secret_word.include?(letter)
+        # "\u{1F7E8}" is the ASCII code for 🟨
+        result[index] = "\u{1F7E8}"
+      end
     end
-
-    # One additional spec for new scenario: wrongly positioned letter
-    it 'should handle a wrongly positioned letter well' do
-      # Given a game object with secret_word 'DRINK'
-      # and guess_word 'CLEAN' (N is a correct letter but in wrong position)
-      game = Game.new('DRINK')
-      guess_word = 'CLEAN'
-
-      # When we try to match the guess word
-      result = game.match_word(guess_word)
-
-      # Then it should return "\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E8}"
-      # which is the ASCII code for 🟦🟦🟦🟦🟨
-      expect(result).to eq("\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E8}")
-    end
+    
+    result
   end
 end
